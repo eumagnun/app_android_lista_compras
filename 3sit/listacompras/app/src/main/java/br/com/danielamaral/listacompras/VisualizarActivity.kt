67bffb1305930ff320b9944936357a69c1733026
logonpflocal <teste@fiap.com>
2022-04-11 23:05:13 -0300

    Update 11/04/2022

---
+++ 3sit/listacompras/app/src/main/java/br/com/danielamaral/listacompras/VisualizarActivity.kt
@@ -0,0 +1,27 @@
+package br.com.danielamaral.listacompras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import br.com.danielamaral.listacompras.model.Produto
+
+class VisualizarActivity : AppCompatActivity() {
+
+    private lateinit var produtoSelecionado: Produto
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visualizar)
+
+        produtoSelecionado = intent.getSerializableExtra("produtoSelecionado") as Produto
+
+        val tvNome:TextView = findViewById(R.id.tvNome)
+        val tvMarca:TextView = findViewById(R.id.tvMarca)
+        val tvPreco:TextView = findViewById(R.id.tvPreco)
+
+        tvNome.text = produtoSelecionado.nome
+        tvMarca.text = produtoSelecionado.marca
+        tvPreco.text = produtoSelecionado.preco.toString()
+
+    }
+}
--- 3sit/listacompras/app/src/main/java/br/com/danielamaral/listacompras/model/Produto.kt
+++ 3sit/listacompras/app/src/main/java/br/com/danielamaral/listacompras/model/Produto.kt
@@ -1,7 +1,9 @@
 package br.com.danielamaral.listacompras.model
 
+import java.io.Serializable
+
 data class Produto(
     val nome: String,
     val marca: String,
     val preco: Double
-)
+):Serializable
